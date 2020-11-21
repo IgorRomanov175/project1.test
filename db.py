939ad61168741969717d39ac8aa47452519c1a57
igor_romanov <igorromanov175@gmail.com>
2020-11-21 21:01:26 +0200

    add new function for heat_transfer_coefficient

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import sqlite3 as sq
 
 with sq.connect('server.db') as con:
     sql = con.cursor()
-    con.row_factory = sq.Row
+    # con.row_factory = sq.Row
     # sql.execute("DROP TABLE users")
 
     sql.execute(""" CREATE TABLE IF NOT EXISTS users (
@@ -22,38 +22,98 @@ with sq.connect('server.db') as con:
 
     con.commit()
 
-    sql.execute(
-        """SELECT internal, external FROM heat_transfer_coefficient 
-        WHERE name_id = 1 """)
-    for result in sql:
-        print(result[1])
+    def heat_transfer_coefficient1():
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient 
+            WHERE name_id = 1 """)
+        for result in sql:
+            print(result[0])
+            
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient 
+            WHERE name_id = 1 """)
+        for result in sql:
+            print(result[0])
 
-    sql.execute(
-        """SELECT internal, external FROM heat_transfer_coefficient 
-        WHERE name_id = 2""")
-    for result in sql:
-        print(result[1])
+    def heat_transfer_coefficient2():
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient 
+            WHERE name_id = 2 """)
+        for result in sql:
+            print(result[1])
+        
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient 
+            WHERE name_id = 2""")
+        for result in sql:
+            print(result[0])
 
-    sql.execute(
-        """SELECT internal, external FROM heat_transfer_coefficient
-        WHERE name_id = '3' """)
-    for result in sql:
-        print(result[1])
+    def heat_transfer_coefficient3():
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 3 """)
+        for result in sql:
+            print(result[1])
+        
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 3 """)
+        for result in sql:
+            print(result[0])
 
-    sql.execute(
-        """SELECT internal, external FROM heat_transfer_coefficient
-        WHERE name_id = '4' """)
-    for result in sql:
-        print(result[1])
+    def heat_transfer_coefficient4():
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 4 """)
+        for result in sql:
+            print(result[1])
+        
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 4 """)
+        for result in sql:
+            print(result[0])
 
-    sql.execute(
-        """SELECT internal, external FROM heat_transfer_coefficient
-        WHERE name_id = '5' """)
-    for result in sql:
-        print(result[1])
+    def heat_transfer_coefficient5():
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 6 """)
+        for result in sql:
+            print(result[1])
 
-    sql.execute(
-        """SELECT internal, external FROM heat_transfer_coefficient
-        WHERE name_id = '6' """)
-    for result in sql:
-        print(result[1])
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 6 """)
+        for result in sql:
+            print(result[0])
+
+    def heat_transfer_coefficient6():
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 6 """)
+        for result in sql:
+            print(result[1])
+
+        sql.execute(
+            """SELECT internal, external FROM heat_transfer_coefficient
+            WHERE name_id = 6 """)
+        for result in sql:
+            print(result[0])
+
+value1 = heat_transfer_coefficient1()
+print(value1)
+
+value2 = heat_transfer_coefficient2()
+print(value2)
+
+value3 = heat_transfer_coefficient3()
+print(value3)
+
+value4 = heat_transfer_coefficient4()
+print(value4)
+
+value5 = heat_transfer_coefficient5()
+print(value5)
+
+value6 = heat_transfer_coefficient6()
+print(value6)
