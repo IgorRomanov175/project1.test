993091a05f7d38e8aa7c83fd579ac517755b7c46
igor_romanov_asus <igorromanov175@gmail.com>
2020-12-04 13:13:06 +0200

    delete logicDBN and completely redesigned logic1 and body1, the first calculation block is running

--- logic1.py
+++ logic1.py
@@ -10,14 +10,21 @@ class Therm1_Math:
 
 
 class Therm1_Logic:
-    def __init__(self, x, y):
-        self.width = x
-        self.thermal_conductivity = y
+    def __init__(self, z1, z2):
+        # self.width = x
+        # self.thermal_conductivity = y
+        self.z1 = z1
+        self.z2 = z2
         self.list = []
 
     def therm_calc(self, x, y):
         self.list.append(Therm1_Math(x, y))
 
+    def dbn_calc(self):
+        return (1 / self.z1) + (1 / self.z2)
 
-#  таски: к методу full_therm_calc подвязать таблицу heat_transfer_coefficient импортировав модуль db в этот файл
-#  методом перебора for i in self.list собрать общее значение термического оперения
+    def full_formul_calc(self):
+        new_formul_calc = self.dbn_calc()
+        for i in self.list:
+            new_formul_calc += i.R_e
+        return new_formul_calc
